39dd0950543c82b8787b85e76c2c3afd1392cf05
ZARA-23 <zarachoudhry325@gmail.com>
2026-09-08 01:49:32 +0500

    refactor: improve server startup log message format

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -42,5 +42,7 @@ mongoose
   });
 
 const startServer = () => {
-  app.listen(PORT,'0.0.0.0', () => console.log(`✅ Server running on http://localhost:${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`✅ Server running on port ${PORT}`);
+  });
 };
